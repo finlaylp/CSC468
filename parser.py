c16fb54740a98d7096c2f20c07212ddafdb220bc
Nikhil <nsridh03@calpoly.edu>
2022-06-05 16:46:23 -0700

    Recommends multiple views

--- parser.py
+++ parser.py
@@ -38,11 +38,11 @@ with open('selects.txt') as f:
 
 sorted_dict = ({k: v for k, v in sorted(count_dict.items(), key=lambda item: item[1])})
 queries = []
-for item in sorted_dict.items():
+for index, item in enumerate(sorted_dict.items()):
     views = ', '.join(item[0].split('|'))
     if(views == ''):
         continue
-    query = '''CREATE VIEW {view_name} AS SELECT * FROM {table_split}'''.format(view_name = "MaterializedView", table_split = views)
+    query = '''CREATE VIEW {view_name} AS SELECT * FROM {table_split}'''.format(view_name = "MaterializedView"+str(index), table_split = views)
     queries.append(query)
 
 
