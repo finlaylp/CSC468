dad1fddfecf9757e9513c4ee7786f220133c6012
Nikhil <nsridh03@calpoly.edu>
2022-06-07 13:23:29 -0700

    Added support for agg functions

--- parser.py
+++ parser.py
@@ -1,6 +1,7 @@
 
 # For Automation purposes / running sql queries in SmallSQL
 import jaydebeapi
+from sklearn.feature_selection import SelectPercentile
 from sql_metadata import Parser
 count_dict = {}
 from collections import Counter
@@ -29,20 +30,22 @@ with open('selects.txt') as f:
     while line:
         line = f.readline()
         join = '|'.join(Parser(line).tables)
+        select_statement = (Parser(line).query.split('FROM')[0]).strip()
         if(count_dict.get(join)):
-            count_dict[join] += 1
+            count_dict[join]['join'] += 1
+            count_dict[join]['select_statement'] = select_statement
         else:
-            count_dict[join] =  1
+            count_dict[join] =  {'join':1, 'select_statement': select_statement}
         curs.close()
 
-
-sorted_dict = ({k: v for k, v in sorted(count_dict.items(), key=lambda item: item[1])})
+sorted_dict = ({k: v for k, v in sorted(count_dict.items(), key=lambda item: item[1]['join'], reverse=True)})
 queries = []
 for index, item in enumerate(sorted_dict.items()):
     views = ', '.join(item[0].split('|'))
+    select_statement = item[1]['select_statement']
     if(views == ''):
         continue
-    query = '''CREATE VIEW {view_name} AS SELECT * FROM {table_split}'''.format(view_name = "MaterializedView"+str(index), table_split = views)
+    query = '''CREATE VIEW {view_name} AS {select_statement} FROM {table_split}'''.format(view_name = "MaterializedView"+str(index), table_split = views, select_statement = select_statement)
     queries.append(query)
 
 
